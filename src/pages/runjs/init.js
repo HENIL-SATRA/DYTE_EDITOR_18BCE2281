4bfba02ff5251d2f77c52a667ba3bd4c5c436166
Henil Satra <53400949+HENIL-SATRA@users.noreply.github.com>
2021-07-17 21:18:16 +0530

    Add files via upload

---
+++ src/pages/runjs/init.js
@@ -0,0 +1,19 @@
+export default {
+	css: `body{
+       
+        width:100%;
+        height:100%;
+    }
+    .blend{
+        position:absolute;
+        left:50%;
+        top:50%;
+        transform:translate(-50%,-50%);
+    }
+    `,
+	html: `
+    <H3 style="margin:30px;">THIS IS THE OUTPUT AREA<H3>`,
+	javascript: `
+    
+`,
+};
---
+++ static/main.js
@@ -0,0 +1,3 @@
+chrome.devtools.panels.create("RunJS", "img/editor.png", "core/index.html", function (panel) {
+	console.log("");
+});
